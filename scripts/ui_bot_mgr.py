b90c3a7ffbb5253f4a0af00ff526c1d2e89bd223
SpenserCai <spensercai@gmail.com>
2023-08-27 22:04:38 +0800

    back

--- scripts/ui_bot_mgr.py
+++ scripts/ui_bot_mgr.py
@@ -3,7 +3,7 @@ Author: SpenserCai
 Date: 2023-08-23 23:07:15
 version: 
 LastEditors: SpenserCai
-LastEditTime: 2023-08-27 21:59:31
+LastEditTime: 2023-08-27 22:03:09
 Description: file content
 '''
 from modules import script_callbacks, paths_internal
@@ -54,10 +54,6 @@ def stop_bot():
         return process_ctrl.ProcessCtrl.LogData + "Stopped\n"
     return process_ctrl.ProcessCtrl.LogData + "Not Running\n"
 
-def TestB():
-    i=[1,2,3]
-    print(i[4])
-
 def discord_tab():
     with gr.Blocks(analytics_enabled=False) as ui:
         with gr.Row():
@@ -76,10 +72,6 @@ def discord_tab():
                 
             with gr.Column():
                 gr.Label("SD-WEBUI-DISCORD LOG")
-                test=gr.Textbox(lines=25, readonly=True, elem_id="test")
-                test.value = "test2"
-                test_button = gr.Button("Test")
-                test_button.click(inputs=[],outputs=[],fn=TestB)
                 log = gr.Textbox(lines=25, readonly=True, elem_id="log_area")
                 start_jscode = """
                 function() {
